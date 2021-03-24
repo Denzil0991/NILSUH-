751382c8b058745d40e4d50c6368181ce3bfd9e4
Denzil0991 <61253340+Denzil0991@users.noreply.github.com>
2021-03-23 20:48:24 -0400

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -1,39 +1,60 @@
 public class Transaction {
+        private String name;
+	private String type;
+	private int transNum;
+	private Date date;
+	private double amount;
 
-	private String name;
-	private long amount;
 
-	   Transaction(String name, long amount)
-	   {
-	      this.name = name;
+	public Transaction(String name, String type,int transNum,Date date,double amount)
+	   {  this.name=name;
+	      this.type = type;
 	      this.amount = amount;
 	   }
 
-	   
 	   String getName()
 	   {
 	      return name;
+	   }
+	   String getType()
+	   {
+	      return type;
 	   }
 
 	   long getAmount()
 	   {
 	      return amount;
 	   }
-	   
 	   void setName(String newName) 
 	   {
 		   this.name = newName;
 	   }
 
-	   void setAmount(long amount)
+	   void setType(String newtype) 
+	   {
+		   this.type = newtype;
+	   }
+
+	   void setAmount(Double amount)
 	   {
 	      this.amount = amount;
 	   }
 	   
-	   void deposit(long amount)
+	   void deposit(Double amount)
+	   {
+	      this.amount += amount;
+	   }
+
+	      void withdrawal(Double amount)
 	   {
 	      this.amount += amount;
 	   }
 	   
-	
+	   void transfer(Double amount)
+	   {
+	      this.amount += amount;
+	   }
+	   
 }
+
+
